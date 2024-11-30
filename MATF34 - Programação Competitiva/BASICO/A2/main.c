ef866def3ddab3d4b7e76d7730e82673b974d9dd
Ernesto Reis <79682382+ernestosrf@users.noreply.github.com>
2024-11-30 18:47:31 -0300

    feat: adding basic exercise 2

--- MATF34 - Programação Competitiva/BASICO/A2/main.c
+++ MATF34 - Programação Competitiva/BASICO/A2/main.c
@@ -3,10 +3,9 @@
 using namespace std;
 
 int main() {
-    int E;
-    int T;
-    cin >> E;
-    cin >> T;
-    cout << E/T;
+    int t1, t2, t3, t4, t5, t6;
+    cin >> t1 >> t2 >> t3 >> t4 >> t5 >> t6;
+    int sum = t1 + t2 + t3 + t4 + t5 + t6;
+    cout << sum << endl;
     return 0;
 }
