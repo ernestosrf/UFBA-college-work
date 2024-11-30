fba1dc3d92ba104bdb3d3cca91b78d4494d03cf5
ernestosrf <ernestosrf98@gmail.com>
2024-11-30 18:46:28 -0300

    feat: adding basic exercise 3

--- MATF34 - Programação Competitiva/BASICO/A3/main.c
+++ MATF34 - Programação Competitiva/BASICO/A3/main.c
@@ -3,10 +3,11 @@
 using namespace std;
 
 int main() {
-    int E;
-    int T;
-    cin >> E;
-    cin >> T;
-    cout << E/T;
+    int Q1, Q2, Q3, E1, E2, E3;
+    cin >> Q1 >> Q2 >> Q3;
+    cin >> E1 >> E2 >> E3;
+    int totalEggsCollected = Q1 + Q2 + Q3;
+    int totalEggsLost = (E1 + E2 + E3) * 3;
+    cout << totalEggsCollected - totalEggsLost << endl;
     return 0;
 }
