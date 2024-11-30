af86304b6f0ed1caafcd8c1925649f515e8a16e1
ernestosrf <ernestosrf98@gmail.com>
2024-11-30 19:12:02 -0300

    feat: creating basic exercise 5

---
+++ MATF34 - Programação Competitiva/BASICO/A5/main.c
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long L, Z, N, U, S;
+    cin >> L >> Z >> N >> U >> S;
+    long sum = L + Z + N + U + S;
+    cout << sum << endl;
+    return 0;
+}
