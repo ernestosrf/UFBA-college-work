f778060b4c63a4398bb70b7cc37a394cf5932b79
ernestosrf <ernestosrf98@gmail.com>
2025-02-01 10:50:22 -0300

    adding basic exercise b4

---
+++ MATF34 - Programação Competitiva/BASICO/B4 - Drone da Amazônia/main.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int X1, X2, Y1, Y2;
+    cin >> X1 >> Y1;
+    cin >> X2 >> Y2;
+
+    if (X1 == X2 && Y1 == Y2) cout << "Soltar pacote";
+    else cout << "Nao soltar pacote";
+    
+    return 0;
+}
