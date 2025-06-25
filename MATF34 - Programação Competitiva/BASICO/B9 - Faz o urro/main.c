815d5a782e4ce3aee6bd21ff0eefb7b6164830d4
Ernesto Reis <79682382+ernestosrf@users.noreply.github.com>
2025-06-25 14:32:43 -0300

    fix: update main.c

--- MATF34 - Programação Competitiva/BASICO/B9 - Faz o urro/main.c
+++ MATF34 - Programação Competitiva/BASICO/B9 - Faz o urro/main.c
@@ -1,11 +1,19 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-    int T, X;
-    cin >> T >> X;
-    int res = X % T;
-    cout << res << endl;
+int main()
+{
+    int A, B, C, SOMA;
+    cin >> A >> B >> C;
+    if(A > 50 || B > 50 || C > 50) {
+        cout << "Shrek vai dar o urro!" << endl;
+        return 0;
+    }
+    
+    SOMA = A + B + C;
+    
+    if(SOMA > 80 || SOMA < 20) cout << "Shrek vai dar o urro!" << endl;
+    else cout << "Shrek ficou calmo." << endl;
+    
     return 0;
 }
