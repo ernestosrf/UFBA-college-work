45243ff0639943a77a30a2cc9119c68fa8476b87
Ernesto Reis <79682382+ernestosrf@users.noreply.github.com>
2025-06-25 14:30:13 -0300

    fix: update main.c

--- MATF34 - Programação Competitiva/BASICO/A7 - Ajude Skywalker/main.c
+++ MATF34 - Programação Competitiva/BASICO/A7 - Ajude Skywalker/main.c
@@ -1,11 +1,11 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    int T, X;
-    cin >> T >> X;
-    int res = X % T;
-    cout << res << endl;
+    int A, B, C, D, E;
+    cin >> A >> B >> C >> D >> E;
+    int TOTAL_ALLY_SHIPS = B+C+D+E;
+    int TOTAL_ENEMY_SHIPS = A - TOTAL_ALLY_SHIPS;
+    cout << TOTAL_ENEMY_SHIPS << endl;
     return 0;
 }
