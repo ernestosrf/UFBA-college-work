094862ca0edc994c3b5b350c3f248feddd0370f3
Ernesto Reis <79682382+ernestosrf@users.noreply.github.com>
2025-06-25 14:31:59 -0300

    fix: update main.c

--- MATF34 - Programação Competitiva/BASICO/B8 - As seis Joias do Infinito/main.c
+++ MATF34 - Programação Competitiva/BASICO/B8 - As seis Joias do Infinito/main.c
@@ -1,11 +1,30 @@
 #include <iostream>
-
 using namespace std;
+#include <algorithm>
+#include <list>
 
-int main() {
-    int T, X;
-    cin >> T >> X;
-    int res = X % T;
-    cout << res << endl;
+int main()
+{
+    int INPUT;
+    list<int> JOIAS = {1,2,3,4,5,6};
+    list <int> JOIAS_RECUPERADAS;
+    
+    for (int i=0; i<6; i++) {
+        cin >> INPUT;
+        JOIAS_RECUPERADAS.push_back(INPUT);
+    }
+    
+    for (int JOIA : JOIAS_RECUPERADAS) {
+        if (std::find(JOIAS.begin(), JOIAS.end(), JOIA) != JOIAS.end()) {
+            JOIAS.remove(JOIA);
+        }
+    }
+    
+    if (JOIAS.empty()) {
+        cout << "Infelizmente perdemos a guerra." << endl;
+    } else {
+        cout << "Os Vingadores ganharam a batalha!!!" << endl;
+    }
+    
     return 0;
 }
