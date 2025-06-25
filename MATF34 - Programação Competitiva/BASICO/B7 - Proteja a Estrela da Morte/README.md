92a0ab0dd596042f97a37e3abdf7a9e7794d0709
ernestosrf <ernestosrf98@gmail.com>
2025-06-25 14:28:31 -0300

    feat: adding new basic exercises

---
+++ MATF34 - Programação Competitiva/BASICO/B7 - Proteja a Estrela da Morte/README.md
@@ -0,0 +1,1 @@
+![image](https://github.com/user-attachments/assets/049f09af-8393-43da-b984-bd9c4a0743cd)
---
+++ MATF34 - Programação Competitiva/BASICO/B7 - Proteja a Estrela da Morte/main.c
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T, X;
+    cin >> T >> X;
+    int res = X % T;
+    cout << res << endl;
+    return 0;
+}
