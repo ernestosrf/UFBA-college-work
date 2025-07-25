ff767050914aaa5d6a342e8c8b3ad1f8902d8d17
Ernesto Reis <79682382+ernestosrf@users.noreply.github.com>
2025-07-25 17:03:33 -0300

    feat: adding basic exercise 8

---
+++ MATF34 - Programação Competitiva/BASICO/A8 - Incursão da Divisão de Reconhecimento/main.c
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    N = N - 20; // numero de titas que o Levi mata sozinho em 1h
+    cout << N/5 << endl;
+    return 0;
+}
